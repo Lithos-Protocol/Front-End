ad7f5b9ea482bf36a6eb33eb3a86790bc9ef8251
ArOhBeK <87249626+ArOhBeK@users.noreply.github.com>
2023-02-21 20:05:25 -0500

    Update YouTube Page

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -27,21 +27,21 @@ const store = useUIStore();
 
 
     <!-- <div>
-      <span class="footer-title">Open Source</span>
-      <a href="https://github.com/Lithos-Protocol" class="link link-hover" target="_blank"
-        rel="noopener noreferrer">GitHub</a>
-      <span class="footer-title">Social</span>
-      <a class="link link-hover" target="_blank" rel="noopener noreferrer"
-        href="https://twitter.com/LITHOSProtocol">Twitter</a>
-      <a href="https://github.com/Lithos-Protocol" class="link link-hover" target="_blank"
-        rel="noopener noreferrer">GitHub</a>
-    </div> -->
-   
+        <span class="footer-title">Open Source</span>
+        <a href="https://github.com/Lithos-Protocol" class="link link-hover" target="_blank"
+          rel="noopener noreferrer">GitHub</a>
+        <span class="footer-title">Social</span>
+        <a class="link link-hover" target="_blank" rel="noopener noreferrer"
+          href="https://twitter.com/LITHOSProtocol">Twitter</a>
+        <a href="https://github.com/Lithos-Protocol" class="link link-hover" target="_blank"
+          rel="noopener noreferrer">GitHub</a>
+      </div> -->
+
 
     <!-- <div>
-      <span class="footer-title">KYA</span>
-      <a class="link link-hover" @click="openKYAModal">Know Your Assumptions</a>
-    </div> -->
+        <span class="footer-title">KYA</span>
+        <a class="link link-hover" @click="openKYAModal">Know Your Assumptions</a>
+      </div> -->
     <img src="/Lithos_MARK3.svg" width="100" height="100" />
   </footer>
 </template>
--- src/components/YouTube.vue
+++ src/components/YouTube.vue
@@ -1,12 +1,11 @@
 <template>
-    <div class="grid gap-4 grid-cols-2">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/x-9x_PveRog"
-            title="SNISP (Succint Non-Interactive Share Proofs) | Ergo Summit - Mining & Minting" frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowfullscreen></iframe>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/VO7_B0e8vSk" title="The Sigma Update -ep9"
+    <div class="grid gap-6 grid-cols-2">
+        <iframe width="540" height="305" src="https://www.youtube.com/embed/x-9x_PveRog" title="SNISP (Succint Non-Interactive Share Proofs) | Ergo Summit - Mining & Minting"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             allowfullscreen></iframe>
-    </div>
-</template>
+        <iframe width="540" height="305" src="https://www.youtube.com/embed/VO7_B0e8vSk" title="The Sigma Update -ep9"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen></iframe>
+</div></template>
