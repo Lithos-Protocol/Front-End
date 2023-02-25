2ca37c0a99baa6dbaf2db60ac84b690ced25e8b6
ArOhBeK <87249626+ArOhBeK@users.noreply.github.com>
2023-02-24 22:15:17 -0500

    Update YouTube.vue

--- src/components/YouTube.vue
+++ src/components/YouTube.vue
@@ -1,11 +1,16 @@
 <template>
-    <div class="grid gap-6 grid-cols-2">
-        <iframe width="540" height="305" src="https://www.youtube.com/embed/x-9x_PveRog" title="SNISP (Succint Non-Interactive Share Proofs) | Ergo Summit - Mining & Minting"
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowfullscreen></iframe>
-        <iframe width="540" height="305" src="https://www.youtube.com/embed/VO7_B0e8vSk" title="The Sigma Update -ep9"
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowfullscreen></iframe>
-</div></template>
+     <div class="grid gap-6 md:gap-12 grid-cols-1 xl:grid-cols-2">
+        <iframe size="cover" src="https://www.youtube.com/embed/x-9x_PveRog" title="SNISP (Succint Non-Interactive Share Proofs) | Ergo Summit - Mining & Minting"
+        width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+            
+        <iframe size="cover" src="https://www.youtube.com/embed/VO7_B0e8vSk" title="The Sigma Update -ep9"
+        width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+</div></template> 
+
+<style>
+iframe {
+	aspect-ratio: 16 / 9;
+	height: 100%;
+	width: 100%;
+}
+</style>
